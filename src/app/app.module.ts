b361e3a75701813cbc7b2d107921c1cfd964013e
hongPhongIT <31528077+hongPhongIT@users.noreply.github.com>
2018-05-14 19:01:03 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
